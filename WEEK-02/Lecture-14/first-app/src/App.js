4b5fcf0f5fe551a2b56b4552cd2dfdcc579bce3b
Ananya <ananya25622singh@gmail.com>
2024-07-03 22:51:37 +0530

    Lec-18 Update

--- WEEK-02/Lecture-14/first-app/src/App.js
+++ WEEK-02/Lecture-14/first-app/src/App.js
@@ -1,22 +1,8 @@
-// eslint-disable-next-line no-unused-vars
-import logo from './logo.svg';
-import './App.css';
 import { Component } from "react";
-
-class App extends Component {
-  NAME = "Ananya!";
-  render() {
-    return (
-      <>
-      <div>
-        <h1>This is {this.NAME}</h1>
-      </div>
-      <div>
-        <p>This is paragraph and name is: {this.NAME}</p>
-      </div>
-      </>
-    );
-  }
+import ToDoScreen from "./screens/ToDoScreen";
+class App extends Component{
+render(){
+    return <ToDoScreen />;
+ }
 }
-
 export default App;
