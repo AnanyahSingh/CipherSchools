d27ae17122a50e79fb3485717512675070a60146
Ananya <ananya25622singh@gmail.com>
2024-07-07 23:10:49 +0530

    Lecture-22 Update

--- WEEK-02/Lecture-14/first-app/src/components/AddTask.js
+++ WEEK-02/Lecture-14/first-app/src/components/AddTask.js
@@ -1,13 +1,21 @@
 const AddTask = () => {
     return (
-        <div className="ui form">
-            <form>
-                <div class="field">
-                    <label>Short Text</label>
-                    <textarea rows="2" spellCheck="false" data-ms-editor="true" ></textarea>
-                </div>
-            </form >
-        </div >
+        <>
+        <h3 className="ui heading center">Add New Task</h3>
+            <div className="ui form">
+                <form>
+                    <div className="field">
+                        <div className="field">
+                            <label>Title</label>
+                            <input type="text" spellCheck="false" data-ms-editor="true" placeholder="Task Title" name="title"/>
+                        </div>
+                        <label>Short Text</label>
+                        <textarea rows="2" spellCheck="false" data-ms-editor="true" placeholder="Task Description" name="desciption"></textarea>
+                    </div>
+                    <button className="ui primary button">Submit</button>
+                </form >
+            </div>
+        </>
     );
 };
 
