31ca713112f3462cdf9bb7938d2ddc6384ed9686
Ananya <ananya25622singh@gmail.com>
2024-07-07 22:53:40 +0530

    Working with Props

---
+++ WEEK-02/Lecture-14/first-app/src/components/AddTask.js
@@ -0,0 +1,14 @@
+const AddTask = () => {
+    return (
+        <div className="ui form">
+            <form>
+                <div class="field">
+                    <label>Short Text</label>
+                    <textarea rows="2" spellCheck="false" data-ms-editor="true" ></textarea>
+                </div>
+            </form >
+        </div >
+    );
+};
+
+export default AddTask;
--- WEEK-02/Lecture-14/first-app/src/screens/ToDoScreen.js
+++ WEEK-02/Lecture-14/first-app/src/screens/ToDoScreen.js
@@ -1,52 +1,32 @@
 import { useState } from "react";
 import Task from "../components/task";
-
-/*class ToDoScreen extends Component {
-state = {
-    taskCount: 0,
-};
-    
-    render() {
-        return (
-            <div className="screen">
-                <h1 className="ui-heading center">To-Do List</h1>
-                <div>
-                    <button onClick={(e) => 
-                    {
-                        this.setState({...this.state, taskCount: this.state.taskCount + 1,});
-                        console.log(this.state.taskCount);
-                    }
-                    } className="ui secondary button">Add Task</button>
-                    <p>Number of tasks: {this.state.taskCount} </p>
-                </div>
-            </div>
-        );
-    }
-}*/
+import AddTask from "../components/AddTask";
 
 const ToDoScreen = () => {
-    const[taskList,setTaskList]=useState([]);
-    return(
+    const [taskList, setTaskList] = useState([]);
+
+    return (
         <div className="screen">
-        <h1 className="ui-heading center">To-Do List</h1>
-        <div>
-            <button onClick={(e) => 
-            {
-                setTaskList([...taskList,{
-                    title: "Go to gym",
-                    description: "Going to gym is good for muscle growth.",
-                    createdDate: new Date(),
-                },
-            ]);
-            }}
-            className="ui secondary button">Add Task</button>
-            <div className="ui cards">
-                {taskList.map((task) => (
-                    <Task task={task} /> //argument for task with value task
-                ))}
+            <h1 className="ui-heading center">To-Do List</h1>
+            <div>
+                <button onClick={() => {
+                    setTaskList([...taskList, {
+                        title: "Go to gym",
+                        description: "Going to gym is good for muscle growth.",
+                        createdDate: new Date(),
+                    }]);
+                }} className="ui secondary button">Add Task</button>
+                <section>
+                    <div className="ui cards">
+                        {taskList.map((task, index) => (
+                            <Task task={task} key={index} />
+                        ))}
+                    </div>
+                </section>
             </div>
+            <AddTask />
         </div>
-    </div>
     );
-}
+};
+
 export default ToDoScreen;
